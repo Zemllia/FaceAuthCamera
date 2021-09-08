59448b82a0376ae1535424ec4a42b229085aa9cc
Daniil <daniil.zemlyanoy@drivepixels.ru>
2021-09-08 12:46:41 +0300

    fixes

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ apt-get install python3-pip cmake libraspberrypi-bin sudo apt-get install python
 apt-get install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test -y
 python3 -m pip install -r requirements.txt
 python3 -m pip install -U numpy
-python3 webstreaming.py
+python3 webstreaming.py -i 192.168.0.104 -o 8000 -f 30
