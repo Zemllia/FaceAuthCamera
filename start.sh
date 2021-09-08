a145f944ad215ef06f48e8c6fce7607b54fc4553
Daniil <daniil.zemlyanoy@drivepixels.ru>
2021-09-08 12:40:09 +0300

    Fix start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-apt-get install python3-pip cmake libraspberrypi-bin  -y
+apt-get install python3-pip cmake libraspberrypi-bin sudo apt-get install python3-h5py -y
+apt-get install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test -y
 python3 -m pip install -r requirements.txt
+python3 -m pip install -U numpy
 python3 webstreaming.py
